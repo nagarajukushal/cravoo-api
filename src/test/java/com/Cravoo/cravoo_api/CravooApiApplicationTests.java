0e04b2694dd6bb0db533648c905745f8fb8d8abf
nagarajukushal <kushalnagaraj1997@gmail.com>
2025-07-02 15:34:10 -0500

    First commit

---
+++ src/test/java/com/Cravoo/cravoo_api/CravooApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Cravoo.cravoo_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CravooApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
